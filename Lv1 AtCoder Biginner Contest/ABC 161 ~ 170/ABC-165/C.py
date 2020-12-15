63ac108d2b4b7ea57b1aed42f169a39aeebdaf5f
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2020-12-15 09:29:59 +0900

    ABC-165 C

--- Lv1 AtCoder Biginner Contest/ABC 161 ~ 170/ABC-165/C.py
+++ Lv1 AtCoder Biginner Contest/ABC 161 ~ 170/ABC-165/C.py
@@ -1,1 +1,13 @@
+import itertools as itr
+
 N,M,Q=map(int,input().split())
+abcd=[list(map(int,input().split()))for i in range(Q)]
+ans=0
+
+for i in itr.combinations_with_replacement(list(range(1,M+1)),N):
+    m=0
+    for a,b,c,d in abcd:
+        if i[b-1]-i[a-1]==c:m+=d
+    ans=max(ans,m)
+
+print(ans)
